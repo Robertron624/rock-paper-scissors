2c4733072ddea01776643c298d6595244da87e26
Roberto Ramirez <robert.624@hotmail.com>
2023-07-21 11:39:53 -0500

    setting up project and custom styles

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-7xl text-rock-start'>
+        Hello World
+      </h1>
+    </>
+  )
+}
+
+export default App
