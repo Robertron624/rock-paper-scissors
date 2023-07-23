7142ad95866c0a9be015b745a6a14936978481f7
Roberto Ramirez <robert.624@hotmail.com>
2023-07-23 14:16:35 -0500

    winner text desktop position

--- src/components/ThirdStep.tsx
+++ src/components/ThirdStep.tsx
@@ -66,7 +66,7 @@ const ThirdStep = () => {
 
     return (
         <div>
-            <div className="choices md:max-w-sm md:h-56 mx-auto h-40 flex justify-between text-white">
+            <div className="choices md:max-w-2xl md:h-56 mx-auto h-40 flex justify-between text-white">
                 <div className="user md:flex-col-reverse flex flex-col justify-between ">
                     
                     {playerChoice === 'rock' && <Rock />}
@@ -77,6 +77,18 @@ const ThirdStep = () => {
 
                     <h3 className="font-semibold">YOU PICKED</h3>
                 </div>
+                <div className="md:block hidden end-message mt-20">
+                <h1 className="text-4xl font-bold text-white">
+
+                    {winner === 'player' && 'YOU WIN'}
+                    {winner === 'computer' && 'YOU LOSE'}
+                    {winner === 'draw' && 'DRAW'}
+
+                </h1>
+                <button onClick={handlePlayAgain} className="mt-10 font-semibold text-sm rounded-md w-44 py-2 text-score bg-white">
+                    PLAY AGAIN
+                </button>
+            </div>
                 <div className="house md:items-center md:flex-col-reverse flex flex-col justify-between items-end">
                     
                     {computerChoice === 'rock' && <Rock/>}
@@ -88,7 +100,7 @@ const ThirdStep = () => {
                     <h3 className="font-semibold">THE HOUSE PICKED</h3>
                 </div>
             </div>
-            <div className="end-message mt-20">
+            <div className="md:hidden end-message mt-20">
                 <h1 className="text-4xl font-bold text-white">
 
                     {winner === 'player' && 'YOU WIN'}
--- src/components/options/Options.tsx
+++ src/components/options/Options.tsx
@@ -3,7 +3,7 @@ export const Rock = () => {
 
     return (
         <div className={`option mx-auto w- bg-gradient-to-b from-rock-start to-rock-end md:w-40 md:h-40 w-28 h-28 rounded-full flex justify-center items-center`}>
-            <div className={`shadow-inner shadow-dark bg-option-background rounded-full $w-24 h-24 md:w-32 md:h-32 flex justify-center items-center`}>
+            <div className={`shadow-inner shadow-dark bg-option-background rounded-full w-24 h-24 md:w-32 md:h-32 flex justify-center items-center`}>
                 <img src="icon-rock.svg" alt="rock" />
             </div>
         </div>
@@ -14,7 +14,7 @@ export const Paper = () => {
 
     return (
         <div className={`option bg-option-background md:w-40 md:h-40 w-28 h-28 rounded-full flex justify-center items-center bg-gradient-to-b from-paper-start to-paper-end`}>
-            <div className={`shadow-inner shadow-dark bg-option-background rounded-full $w-24 h-24 md:w-32 md:h-32 flex justify-center items-center`}>
+            <div className={`shadow-inner shadow-dark bg-option-background rounded-full w-24 h-24 md:w-32 md:h-32 flex justify-center items-center`}>
                 <img src="icon-paper.svg" alt="paper" />
             </div>
         </div>
@@ -33,7 +33,7 @@ export const Scissors = () => {
 
 export const Placeholder = () => {
     return (
-        <div className="w-28 h-28 md:w-40 md:h-40  rounded-full flex items-center">
+        <div className="w-28 h-28 md:w-40 md:h-40 rounded-full flex items-center">
             <div className="animate-pulse option bg-placeholder w-24 h-24 md:w-32 md:h-32 rounded-full flex justify-center items-center ml-auto">
             </div>
         </div>
