d56db4cba1966b7aceda50858ad11709c295e992
Roberto Ramirez <robert.624@hotmail.com>
2023-07-23 22:41:05 -0500

    fix: close modal with scape key and click outside

--- src/components/FirstStep.tsx
+++ src/components/FirstStep.tsx
@@ -18,11 +18,11 @@ const FirstStep = () => {
     return (
         <div className="md:max-w-sm md:gap-12 options mx-auto flex flex-col bg-triangle bg-no-repeat bg-center bg-[length:15rem_12rem] md:bg-[length:22rem_14rem]">
             <div className="flex justify-between">
-                <button onClick={handlePlayerChoice} id="paper">
+                <button className="hover:scale-110 transition-all" onClick={handlePlayerChoice} id="paper">
                     <span className="sr-only">paper</span>
                     <Paper />
                 </button>
-                <button onClick={handlePlayerChoice} id="scissors">
+                <button className="hover:scale-110 transition-all" onClick={handlePlayerChoice} id="scissors">
                     <span className="sr-only">scissors</span>
                     <Scissors />
                 </button>
@@ -30,7 +30,7 @@ const FirstStep = () => {
             <button
                 onClick={handlePlayerChoice}
                 id="rock"
-                className="mx-auto w-fit"
+                className="mx-auto w-fit hover:scale-110 transition-all"
             >
                 <span className="sr-only">rock</span>
                 <Rock />
--- src/components/RulesModal.tsx
+++ src/components/RulesModal.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 interface RulesModalProps {
     isRulesModalOpen: boolean;
     setIsRulesModalOpen: (isRulesModalOpen: boolean) => void;
@@ -11,18 +13,57 @@ const RulesModal = ({
         setIsRulesModalOpen(!isRulesModalOpen);
     };
 
+    
+    useEffect(() => {
+        // Close the modal when the user presses the escape key
+        const handleEscape = (e: KeyboardEvent) => {
+            if (e.key === "Escape") {
+                setIsRulesModalOpen(false);
+            }
+        };
+        window.addEventListener("keydown", handleEscape);
+
+        // Close the modal when the user clicks outside of it
+        const handleClickOutside = (e: MouseEvent) => {
+            const target = e.target as HTMLElement;
+            if (target?.classList?.contains("modal")) {
+                setIsRulesModalOpen(false);
+            }
+        };
+        window.addEventListener("click", handleClickOutside);
+
+        return () => {
+            window.removeEventListener("keydown", handleEscape);
+            window.removeEventListener("click", handleClickOutside);
+        }
+    }, [setIsRulesModalOpen]);
+    
+
     return (
-        <dialog open={isRulesModalOpen} className="modal">
-            <div className="modal-content h-full w-full bg-white py-14">
-                <h2 className="mb-20 text-3xl font-bold text-dark">RULES</h2>
-                <div className="modal-body flex justify-center">
-                    <img src="/image-rules.svg" alt="rules" />
-                </div>
-                <button className="mt-28" onClick={handleRulesModal}>
+        <dialog open={isRulesModalOpen} className="modal backdrop-brightness-50">
+            <div className="modal-box max-w-none max-h-none h-full w-full bg-white py-14 md:py-6 md:max-w-md md:max-h-[27rem] rounded-md">
+                <div className="mb-20 md:flex md:justify-between justify-center w-full md:max-w-[90%] md:mx-auto">
+                <h2 className="text-3xl font-bold text-dark">RULES</h2>
+                <button className="hidden md:block" onClick={handleRulesModal}>
                     <span className="sr-only">
                         close
                     </span>
                     <img src="/icon-close.svg" alt="close" />
+                </button>
+                </div>
+                <div className="modal-body flex justify-center">
+                    <img src="/image-rules.svg" alt="rules" />
+                </div>
+                <button className="mt-28 md:hidden" onClick={handleRulesModal}>
+                    <span className="sr-only">
+                        close
+                    </span>
+                    <img src="/icon-close.svg" alt="close" />
+                </button>
+            </div>
+            <div className="modal-backdrop">
+                <button>
+                    close
                 </button>
             </div>
         </dialog>
